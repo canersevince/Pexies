6ea5745e6d02edc649e23a6bec0b29e90f067938
canersevince <canersevince@Caners-MacBook-Pro.local>
2020-08-30 00:47:13 +0300

    init

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,6 @@ import * as DotEnv from 'dotenv';
 DotEnv.config()
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
